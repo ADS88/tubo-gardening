ee8ba389881064facd7bf0b2e80d0f36403aabc4
Andrew Sturman <andrews@Andrews-MacBook-Pro.local>
2025-08-08 10:48:48 +1000

    Initial commit

---
+++ Tubo Gardening/Tubo_GardeningApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tubo_GardeningApp.swift
+//  Tubo Gardening
+//
+//  Created by Andrew Sturman on 8/8/2025.
+//
+
+import SwiftUI
+
+@main
+struct Tubo_GardeningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
